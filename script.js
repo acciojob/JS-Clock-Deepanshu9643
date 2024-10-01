f59e28d8a81c3954c4617a8aa97ba4760851e514
Deepanshu9643 <deepanshukhorwal7@gmail.com>
2024-10-02 00:01:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 //your code here
+function setDate() {
+    const now = new Date();
+    const seconds = now.getSeconds();
+    const minutes = now.getMinutes();
+    const hours = now.getHours();
+
+   
+    const secondDegrees = ((seconds / 60) * 360) + 90; // Offset for CSS rotation
+    const minuteDegrees = ((minutes / 60) * 360) + ((seconds / 60) * 6) + 90; // Offset for CSS rotation
+    const hourDegrees = ((hours / 12) * 360) + ((minutes / 60) * 30) + 90; // Offset for CSS rotation
+
+    document.querySelector('.second-hand').style.transform = `rotate(${secondDegrees}deg)`;
+    document.querySelector('.min-hand').style.transform = `rotate(${minuteDegrees}deg)`;
+    document.querySelector('.hour-hand').style.transform = `rotate(${hourDegrees}deg)`;
+}
+
+setInterval(setDate, 1000);
+setDate(); 
